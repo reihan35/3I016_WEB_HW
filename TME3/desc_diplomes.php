1581205e292e888b6ec7e8ebeaa8dfb776fcd4d4
laudao <nguyen.laura97@gmail.com>
2017-10-04 17:44:53 +0200

    TME3

---
+++ TME3/desc_diplomes.php
@@ -0,0 +1,14 @@
+<?php
+$desc_diplomes = 
+    array(
+        'Licence' => array(
+            'L1' => array('1I101', '1I102', '1I112'),
+            'L2' => array('2I111', '2I122', '2I130', '2I135'),
+            'L3' => array('3I016'),
+        ),
+        'Master' => array(
+            'M1' => array('4I400', '4I401'),
+            'M2' => array('5I500', '5I510', '5I520')
+        )
+    );
+?>
